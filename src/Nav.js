c65a5fb4e8646c654a1f799a688976f00bc5a714
chukkydave <chukkydave@gmail.com>
2021-01-27 10:15:06 +0100

    first commit

---
+++ src/Nav.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import {link} from 'react-router-dom'
+
+class Nav extends Component{
+    render() {
+        return (
+            <div>
+                <link to={}></link>
+            </div>
+        )
+    }
+}
